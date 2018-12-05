ee977d1316729d6979b631531fdd999f519e2b0f
Adi <adikur20@gmail.com>
2018-12-05 10:19:38 +0700

    init

--- src/main/java/com/example/toasterlibrary/ToasterMessage.java
+++ src/main/java/com/example/toasterlibrary/ToasterMessage.java
@@ -8,6 +8,7 @@ public class ToasterMessage {
     public static void s(Context c, String message){
 
         Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
 
     }
 }
